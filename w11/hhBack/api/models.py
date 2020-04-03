d97ea4a42b59b0bebfe29488e019fc44e82841ba
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-03 07:38:41 +0600

    head hunter

---
+++ w11/hhBack/api/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(default='')
+    city = models.CharField(max_length=100, default='')
+    address = models.TextField(default='')
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'city': self.city,
+            'address': self.address
+        }
+
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    salary = models.FloatField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary,
+        }
---
+++ w11/hhBack/api/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.http.response import JsonResponse
+from api.models import Company
+from api.models import Vacancy
+
+def companies_list(request):
+    companies = Company.objects.all()
+    companies_json = [company.to_json() for company in companies]
+    return JsonResponse(companies_json, safe=False)
+
+def company_details(request, company_id):
+    try:
+        company = Company.objects.get(id = company_id)
+    except Company.DoesNotExist as e:
+        return JsonResponse({'error': str(e)})
+    return JsonResponse(company.to_json())
+
+def company_vacancies(request, company_id):
+    try:
+        c = Company.objects.get(id=company_id)
+    except Company.DoesNotExist as e:
+        return JsonResponse({'error': str(e)})
+    vacancies = Vacancy.objects.filter(company=c)
+    json_vacancies = [v.to_json() for v in vacancies]
+    return JsonResponse(json_vacancies, safe=False)
+
+def vacancies_list(request):
+    vacancies = Vacancy.objects.all()
+    json_vacancies = [v.to_json() for v in vacancies]
+    return JsonResponse(json_vacancies, safe=False)
+
+
+def vacancy_details(request, vacancy_id):
+    try:
+        vacancy = Vacancy.objects.get(id=vacancy_id)
+    except Vacancy.DoesNotExist as e:
+        return JsonResponse({'error': str(e)})
+    return JsonResponse(vacancy.to_json())
+
+
+def top_vacancies_by_salary(request):
+    vacancies = Vacancy.objects.order_by('-salary')[:10]
+    json_vacancies = [v.to_json() for v in vacancies]
+    return JsonResponse(json_vacancies, safe=False)
+# Create your views here.
